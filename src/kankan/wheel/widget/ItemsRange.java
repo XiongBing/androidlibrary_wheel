f6dbffec16b1197b1b3c851d24050d257cc18f61
Xiong Bing <xiongbing@chinamobo.com>
2014-08-19 10:48:57 +0800

    delete all 10 changes

--- src/kankan/wheel/widget/ItemsRange.java
+++ src/kankan/wheel/widget/ItemsRange.java
@@ -19,16 +19,6 @@
 //added 3rd change in project2 lib submodule
 package kankan.wheel.widget;
 
-//change 1 
-//change 2
-//change 3
-//change 4 in project3
-//change 5 in project2
-//change 6 
-//change 7
-//change 8
-//change 9
-//change 10 
 /**
  * Range for visible items.
  */ 
